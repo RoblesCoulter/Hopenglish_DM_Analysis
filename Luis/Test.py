33e70edb7c6b195b7fb1dc18405f2c1f53eaf366
Luis Robles <roblescoulter@gmail.com>
2016-12-02 14:20:15 +0800

    Project Description

---
+++ Luis/Test.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+videoInfo = pd.read_csv("videoDataInfo.csv")
+studentBehaviorInfo_1 = pd.read_json("studentBehaviorInfo_1.json")
+studentBehaviorInfo_2 = pd.read_json("studentBehaviorInfo_2.json")
