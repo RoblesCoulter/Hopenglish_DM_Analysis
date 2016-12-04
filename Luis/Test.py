689f0a52332b5983102d0658e03fa0fdc967510d
Luis Robles <roblescoulter@gmail.com>
2016-12-04 22:26:32 +0800

    Video Counts

--- Luis/Test.py
+++ Luis/Test.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-videoInfo = pd.read_csv("/Users/Dev/Hopenglish_DM_Analysis/Luis/videoDataInfo.csv")
-studentBehaviorInfo_1 = pd.read_json("studentBehaviorInfo_1.json")
-studentBehaviorInfo_2 = pd.read_json("studentBehaviorInfo_2.json")
+videoInfo = pd.read_csv("/Users/roblescoulter/Dev/Hopenglish_DM_Analysis/videoDataInfo.csv")
+studentBehaviorInfo_1 = pd.read_json("/Users/roblescoulter/Dev/Hopenglish_DM_Analysis/studentBehaviorInfo_1.json")
+studentBehaviorInfo_2 = pd.read_json("/Users/roblescoulter/Dev/Hopenglish_DM_Analysis/studentBehaviorInfo_2.json")
